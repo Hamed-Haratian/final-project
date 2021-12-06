b86344f7d3b3c52466bb92185f82efb375b3d471
Hamed Haratian <hamed.haratian@yahoo.com>
2021-12-05 19:41:15 -1200

    final project

---
+++ Js/app.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 40,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop: true,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+  });
